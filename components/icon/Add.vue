959ff7336edc1273181c2c3582562bf4e5ca68b4
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2023-07-08 09:18:28 +0100

    (chore): icon components

---
+++ components/icon/Add.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M3.86561 10V0H6.13439V10H3.86561ZM0 6.13439V3.86561H10V6.13439H0Z" fill="black" fill-opacity="0.8" />
+    </svg>
+</template>
+
