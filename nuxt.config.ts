f9f9abe42a1c84a07d011a6e7d3d52bd729bce5a
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2023-07-08 09:36:14 +0100

    (chore): scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      title: "SmartPass",
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          charset: "utf-8",
+        },
+        {
+          name: "description",
+          content:
+            "SmartPass is a password manager that helps you to manage your passwords and keep them safe.",
+        },
+      ],
+    },
+  },
+  css: ["~/assets/scss/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/scss/core/_variables.scss"; @import "@/assets/scss/utils/_mixins.scss";`,
+        },
+      },
+    },
+  },
+  devtools: { enabled: true },
+});
