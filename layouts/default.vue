9462b413dfcad141d512357c349fc4b3873cbdf3
Abdullahi Odesanmi <abdulodesanmi@gmail.com>
2023-07-09 01:20:55 +0100

    (chore): added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const showToast = ref(false);
+const toastMessage = ref("");
+
+useListen("showToast", (message: string) => {
+    showToast.value = true;
+    toastMessage.value = message;
+    setTimeout(() => {
+        showToast.value = false;
+        toastMessage.value = "";
+    }, 3000);
+});
+</script>
+
+<template>
+    <slot />
+    <!-- TOAST NOTIFICATION -->
+    <BaseToast v-if="showToast" :message="toastMessage" />
+</template>
